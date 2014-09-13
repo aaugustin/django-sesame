fc5e80eb959fc054698d10bdda7f2f4b5c71430a
Aymeric Augustin <aymeric.augustin@m4x.org>
2014-09-13 14:58:04 +0200

    Clean up flake8 output.

--- sesame/tests.py
+++ sesame/tests.py
@@ -3,9 +3,9 @@ from __future__ import unicode_literals
 import django
 
 if django.VERSION[:2] < (1, 6):     # unittest-style discovery isn't available
-    from .test_backends import TestModelBackend
-    from .test_middleware import TestAfterAuthMiddleware
-    from .test_middleware import TestBeforeAuthMiddleware
-    from .test_middleware import TestWithoutAuthMiddleware
-    from .test_middleware import TestWithoutSessionMiddleware
-    from .test_utils import TestUtils
+    from .test_backends import TestModelBackend                         # noqa
+    from .test_middleware import TestAfterAuthMiddleware                # noqa
+    from .test_middleware import TestBeforeAuthMiddleware               # noqa
+    from .test_middleware import TestWithoutAuthMiddleware              # noqa
+    from .test_middleware import TestWithoutSessionMiddleware           # noqa
+    from .test_utils import TestUtils                                   # noqa

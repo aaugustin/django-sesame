141a23068f505546536518afc51d131c6a948e66
Aymeric Augustin <aymeric.augustin@m4x.org>
2016-09-03 20:29:36 +0200

    Simplify packaging according to PyPA guidelines.

--- setup.py
+++ setup.py
@@ -1,51 +1,46 @@
+from __future__ import unicode_literals
+
+import codecs
 import os.path
 
 import setuptools
 
-# Avoid polluting the .tar.gz with ._* files under Mac OS X
-os.putenv('COPYFILE_DISABLE', 'true')
+root_dir = os.path.abspath(os.path.dirname(__file__))
 
-# Prevent setuptools from complaining that a standard file wasn't found
-README = os.path.join(os.path.dirname(__file__), 'README')
-if not os.path.exists(README):
-    os.symlink(README + '.rst', README)
+description = ("URL-based authentication, an application that provides "
+               "one-click login via specially crafted URLs")
 
-description = ('URL-based authentication, an application that provides '
-               'one-click login via specially crafted URLs')
-
-with open(README) as f:
-    long_description = '\n\n'.join(f.read().split('\n\n')[2:8])
+with codecs.open(os.path.join(root_dir, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
 
 setuptools.setup(
     name='django-sesame',
     version='1.2',
-    author='Aymeric Augustin',
-    author_email='aymeric.augustin@m4x.org',
-    url='https://github.com/aaugustin/django-sesame',
     description=description,
     long_description=long_description,
-    download_url='http://pypi.python.org/pypi/django-sesame',
+    url='https://github.com/aaugustin/django-sesame',
+    author='Aymeric Augustin',
+    author_email='aymeric.augustin@m4x.org',
+    license='BSD',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Framework :: Django :: 1.8',
+        'Framework :: Django :: 1.9',
+        'Framework :: Django :: 1.10',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
     packages=[
         'sesame',
     ],
-    classifiers=[
-        "Development Status :: 5 - Production/Stable",
-        "Environment :: Web Environment",
-        "Framework :: Django",
-        "Framework :: Django :: 1.8",
-        "Framework :: Django :: 1.9",
-        "Framework :: Django :: 1.10",
-        "Intended Audience :: Developers",
-        "License :: OSI Approved :: BSD License",
-        "Operating System :: OS Independent",
-        "Programming Language :: Python",
-        "Programming Language :: Python :: 2",
-        "Programming Language :: Python :: 2.7",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.3",
-        "Programming Language :: Python :: 3.4",
-        "Programming Language :: Python :: 3.5",
-    ],
-    platforms='all',
-    license='BSD'
 )

3f23f97c99cab2db091ebc0f4edbb5b2463f8a5b
Aymeric Augustin <aymeric.augustin@m4x.org>
2019-04-15 22:24:20 +0200

    Add files forgotten in previous commit.

---
+++ sesame/test_app/models.py
@@ -0,0 +1,19 @@
+from __future__ import unicode_literals
+
+import uuid
+
+from django.contrib.auth import models as auth_models
+from django.db import models
+
+
+class UUIDUser(auth_models.AbstractBaseUser):
+    id = models.UUIDField(default=uuid.uuid4, primary_key=True)
+    username = models.CharField(max_length=32, unique=True)
+
+    USERNAME_FIELD = 'username'
+
+
+class CharUser(auth_models.AbstractBaseUser):
+    username = models.CharField(max_length=32, primary_key=True)
+
+    USERNAME_FIELD = 'username'

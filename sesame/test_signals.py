9d84c495ae2472f9f268a1bab377ef49ac54a109
Aymeric Augustin <aymeric.augustin@m4x.org>
2019-05-01 09:03:07 +0200

    Remove unused code (YAGNI).

--- sesame/test_signals.py
+++ sesame/test_signals.py
@@ -11,14 +11,7 @@ from .backends import UrlAuthBackendMixin
 def reset_sesame_settings(**kwargs):
     UABM = UrlAuthBackendMixin
 
-    if kwargs['setting'] == 'SESAME_SALT':
-        UABM.salt = getattr(settings, 'SESAME_SALT', 'sesame')
-    elif kwargs['setting'] == 'SESAME_DIGEST':
-        UABM.digest = getattr(settings, 'SESAME_DIGEST', hashlib.md5)
-    elif kwargs['setting'] == 'SESAME_ITERATIONS':
-        UABM.iterations = getattr(settings, 'SESAME_ITERATIONS', 10000)
-
-    elif kwargs['setting'] == 'SESAME_MAX_AGE':
+    if kwargs['setting'] == 'SESAME_MAX_AGE':
         UABM.max_age = getattr(settings, 'SESAME_MAX_AGE', None)
     elif kwargs['setting'] == 'SESAME_ONE_TIME':
         UABM.one_time = getattr(settings, 'SESAME_ONE_TIME', False)

576e2b56931b6b56f486d49755c9bda747742417
Aymeric Augustin <aymeric.augustin@m4x.org>
2019-06-15 12:11:06 +0200

    Confirm that distinct users never get distinct tokens.
    Empty or identical revocation keys don't cause issues (beyond the
    inability to revocate tokens if they're empty).
    
    Fix #34.

--- sesame/test_backends.py
+++ sesame/test_backends.py
@@ -5,6 +5,7 @@ import io
 import logging
 
 from django.contrib.auth import get_user_model
+from django.core import signing
 from django.core.exceptions import ImproperlyConfigured
 from django.test import TestCase, override_settings
 from django.utils import timezone
@@ -82,6 +83,45 @@ class TestModelBackend(TestCase):
         with self.assertRaises(TypeError):
             self.backend.authenticate(request=None, url_auth_token=None)
         self.assertIn("TypeError", self.get_log())
+
+    def test_naive_token_hijacking_fails(self):
+        # Tokens contain the PK of the user, the hash of the revocation key,
+        # and a signature. The revocation key may be identical for two users:
+        # - if SESAME_INVALIDATE_ON_PASSWORD_CHANGE is False or if they don't
+        #   have a password;
+        # - if SESAME_ONE_TIME is False or if they have the same last_login.
+        similar_user = type(self.user).objects.create(
+            username="jane", last_login=self.user.last_login
+        )
+
+        token1 = self.backend.create_token(self.user)
+        token2 = self.backend.create_token(similar_user)
+        self.backend.unsign(token1)
+        self.backend.unsign(token2)
+
+        # Check that the test scenario produces identical revocation keys.
+        # This test depends on the implementation of django.core.signing;
+        # however, the format of tokens must be stable to keep them valid.
+        data1, sig1 = token1.split(self.backend.signer.sep, 1)
+        data2, sig2 = token2.split(self.backend.signer.sep, 1)
+        bin_data1 = signing.b64_decode(data1.encode())
+        bin_data2 = signing.b64_decode(data2.encode())
+        pk1 = self.backend.packer.pack_pk(self.user.pk)
+        pk2 = self.backend.packer.pack_pk(similar_user.pk)
+        self.assertEqual(bin_data1[: len(pk1)], pk1)
+        self.assertEqual(bin_data2[: len(pk2)], pk2)
+        key1 = bin_data1[len(pk1) :]
+        key2 = bin_data2[len(pk2) :]
+        self.assertEqual(key1, key2)
+
+        # Check that changing just the PK doesn't allow hijacking the other
+        # user's account -- because the PK is included in the signature.
+        bin_data = pk2 + key1
+        data = signing.b64_encode(bin_data).decode()
+        token = data + sig1
+        user = self.backend.parse_token(token)
+        self.assertEqual(user, None)
+        self.assertIn("Bad token", self.get_log())
 
 
 @override_settings(SESAME_MAX_AGE=10)

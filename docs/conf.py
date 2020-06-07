f8c22c35088293f899da263db0f7e74955101281
Aymeric Augustin <aymeric.augustin@m4x.org>
2020-06-07 13:10:48 +0200

    Apply black to conf.py.

--- docs/conf.py
+++ docs/conf.py
@@ -9,12 +9,12 @@ import datetime
 
 # -- Project information -----------------------------------------------------
 
-project = 'django-sesame'
-copyright = '2012-{}, Aymeric Augustin'.format(datetime.date.today().year)
-author = 'Aymeric Augustin'
+project = "django-sesame"
+copyright = "2012-{}, Aymeric Augustin".format(datetime.date.today().year)
+author = "Aymeric Augustin"
 
 # The full version, including alpha/beta/rc tags
-release = '2.0'
+release = "2.0"
 
 
 # -- General configuration ---------------------------------------------------
@@ -23,29 +23,29 @@ release = '2.0'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    'sphinxcontrib.spelling',
+    "sphinxcontrib.spelling",
 ]
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ['_build']
+exclude_patterns = ["_build"]
 
 # Workaround for https://github.com/readthedocs/readthedocs.org/issues/2569.
-master_doc = 'index'
+master_doc = "index"
 
 
 # -- Options for HTML output -------------------------------------------------
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'alabaster'
+html_theme = "alabaster"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = ["_static"]
 
-html_css_files = ['custom.css']
+html_css_files = ["custom.css"]
 
-html_sidebars = {'**': []}
+html_sidebars = {"**": []}

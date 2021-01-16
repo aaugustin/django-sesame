dde94270a43b48c401ce84da9cd9c94c882ff6ae
Aymeric Augustin <aymeric.augustin@m4x.org>
2021-01-16 14:17:19 +0100

    Bump version number.

--- docs/conf.py
+++ docs/conf.py
@@ -14,7 +14,7 @@ copyright = "2012-{}, Aymeric Augustin".format(datetime.date.today().year)
 author = "Aymeric Augustin"
 
 # The full version, including alpha/beta/rc tags
-release = "2.1"
+release = "2.2"
 
 
 # -- General configuration ---------------------------------------------------

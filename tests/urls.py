0e0cd3115da7e2edfa80b3c0beda07a1fee866a3
Aymeric Augustin <aymeric.augustin@m4x.org>
2022-05-06 08:04:50 +0200

    Separate test view from test URLconf.

--- tests/urls.py
+++ tests/urls.py
@@ -1,20 +1,7 @@
-from django.http import HttpResponse
-from django.template import engines
 from django.urls import re_path
 
+from .views import show_user
 
-def show_user(request):
-    content = (
-        engines["django"]
-        .from_string(
-            "{% if user.is_authenticated %}{{ user }}"
-            "{% elif user.is_anonymous %}anonymous"
-            "{% else %}no user"
-            "{% endif %}"
-        )
-        .render(request=request)
-    )
-    return HttpResponse(content, content_type="text/plain")
-
-
-urlpatterns = [re_path(r"", show_user)]
+urlpatterns = [
+    re_path("", show_user),  # catchall pattern
+]
---
+++ tests/views.py
@@ -0,0 +1,16 @@
+from django.http import HttpResponse
+from django.template import engines
+
+
+def show_user(request):
+    content = (
+        engines["django"]
+        .from_string(
+            "{% if user.is_authenticated %}{{ user }}"
+            "{% elif user.is_anonymous %}anonymous"
+            "{% else %}no user"
+            "{% endif %}"
+        )
+        .render(request=request)
+    )
+    return HttpResponse(content, content_type="text/plain")

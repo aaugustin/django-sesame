8ba76d5fc45e2de034caaf4c235f7a7e8429f858
Aymeric Augustin <aymeric.augustin@m4x.org>
2022-06-06 12:29:58 +0200

    Add button to copy code samples.

--- docs/conf.py
+++ docs/conf.py
@@ -23,7 +23,9 @@ release = "2.4"
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = []
+extensions = [
+    "sphinx_copybutton",
+]
 if "spelling" in sys.argv:
     extensions.append("sphinxcontrib.spelling")
 

f004f3f322d7c73a5dbc38de7d245bd10808bcbc
Aymeric Augustin <aymeric.augustin@m4x.org>
2023-08-09 08:24:22 +0200

    Tag version 3.2.1.

--- docs/conf.py
+++ docs/conf.py
@@ -34,7 +34,7 @@ copyright = f"2012-{datetime.date.today().year}, Aymeric Augustin and contributo
 author = "Aymeric Augustin"
 
 # The full version, including alpha/beta/rc tags
-release = "3.2"
+release = "3.2.1"
 
 
 # -- General configuration ---------------------------------------------------
